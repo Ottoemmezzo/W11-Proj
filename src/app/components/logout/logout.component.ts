477249d6f624d29e83031f088f90be5c853147eb
Sebastian Lazzeri <sebastiano.lazzeri@gmail.com>
2022-12-12 10:22:22 +0100

    mo

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,13 +9,18 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./logout.component.scss']
 })
 export class LogoutComponent implements OnInit,OnChanges {
+  user$ = this.authSrv.authSubject.asObservable()
+  //this.authSrv.isLoggedIn$ = this.user$.pipe(map(u=>!!u))
 
   constructor(private router:Router, private authSrv:AuthService) { }
 
   ngOnInit(): void {
     localStorage.removeItem('UserData');
     localStorage.removeItem('user');
-    this.authSrv.isLoggedIn$.subscribe(ob=> ob=false);
+    this.authSrv.isLoggedIn$ = this.user$.pipe(map(u=>!!u))
+
+    //this.authSrv.isLoggedIn$ =false; //this.authSrv.user$.pipe(map(u=>!!!u))
+    //this.authSrv.isLoggedIn$.pipe(map(ob=> ob=false)).subscribe();
     this.router.navigate(['/login'])
 
 
